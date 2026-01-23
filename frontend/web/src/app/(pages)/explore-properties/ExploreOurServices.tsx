5cd49c72a55b0bd3e35222a5228934bbcfb9ea77
muzeef999 <shaikmuzeef9999@gmail.com>
2026-01-23 15:08:22 +0530

    added spacing and design

--- frontend/web/src/app/(pages)/explore-properties/ExploreOurServices.tsx
+++ frontend/web/src/app/(pages)/explore-properties/ExploreOurServices.tsx
@@ -40,7 +40,7 @@ export default function ExploreOurServices() {
   const { selectedCity } = useCity();
   return (
     <>
-      <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+      <div className="flex flex-col gap-3 m-0 p-0 sm:flex-row sm:items-center sm:justify-between">
         <div className="headingSideBar">
           <h1 className="text-base font-bold sm:text-2xl truncate">
             Explore Our Services
