bf84690ffddce2169c9a5edc4ce76c13eb2403aa
muzeef999 <shaikmuzeef9999@gmail.com>
2026-01-30 17:40:29 +0530

    worked on payment route

--- frontend/web/src/components/Banner.tsx
+++ frontend/web/src/components/Banner.tsx
@@ -42,28 +42,33 @@ const Banner = () => {
         {/* Overlay */}
         <div className="absolute inset-0 flex items-center">
           <div className="w-full md:w-[65%]  px-6 md:px-14 py-10 space-y-4">
-            {/* Badge */}
-            <div className="flex items-center gap-2 px-4 py-2 bg-[#27AE60] backdrop-blur-md rounded-full w-fit shadow-sm">
-              <IoHomeOutline className="w-4 h-4 text-white" />
-              <span className="text-sm font-semibold text-white tracking-wide">
+            <div
+              className="inline-flex items-center gap-2 px-4 py-2 rounded-full
+  bg-white/90 backdrop-blur-lg
+  border border-[#27AE60]/30
+  shadow-[0_8px_30px_rgba(39,174,96,0.18)]
+"
+            >
+              <IoHomeOutline className="w-4 h-4 text-[#27AE60]" />
+              <span className="text-sm font-semibold text-[#27AE60] tracking-wide">
                 Trusted Real Estate Platform
               </span>
             </div>
 
             <h1 className="text-neutral-600 font-bold text-4xl">
-              Looking for a <span className="highlight-truly">better</span>{" "}
-              property experience?
+              Are you looking for a happy home? <br />
+              just like we did?
             </h1>
             <h3 className="text-neutral-600 font-normal text-xl">
-              Tired of fake listings, spam calls, and wasted site visits?
+              Tired of fake listings and spam calls?
             </h3>
 
             <div className="h-[3.2rem] flex items-center gap-3 overflow-hidden">
               {/* check icon */}
 
               <p className="text-slate-600 text-lg whitespace-nowrap">
-                That’s why we built{" "}
-                <span className="highlight-name-logo">PROPENU</span>.
+                Introducing <span className="highlight-name-logo">PROPENU</span>
+                .
               </p>
               {/* animated trust text */}
               <IoMdCheckmarkCircleOutline
@@ -99,17 +104,16 @@ const Banner = () => {
         <div className="absolute inset-0 flex items-start">
           <div className="w-full  px-5 py-8 space-y-3">
             <h1 className="text-neutral-600 font-bold  text-2xl">
-              Looking for a <span className="highlight-truly">better</span>{" "}
-              property experience?
+              Are you looking for a happy home? just like we did?
             </h1>
 
             <h3 className="text-neutral-600 font-normal text-xl">
-              Tired of fake listings, spam calls, and wasted site visits?
+              Tired of fake listings and spam calls?
             </h3>
 
             <div className="h-[5.2rem] flex flex-col overflow-hidden">
               <p className="text-2xl">
-                That’s why we built{" "}
+                That's when we found{" "}
                 <span className="highlight-name-logo">PROPENU</span>.
               </p>
               {/* animated trust text */}
