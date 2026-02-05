2dabbe4ac5402a9b268f51f1b7d20141c4f51970
muzeef999 <shaikmuzeef9999@gmail.com>
2026-02-05 19:16:22 +0530

    added  pdf data

--- backend/services/payment-service/src/controller/subscriptionController.ts
+++ backend/services/payment-service/src/controller/subscriptionController.ts
@@ -121,8 +121,7 @@ export async function getSubscriptionHistory(req: AuthRequest, res: Response) {
         startDate: item.startDate,
         endDate: item.endDate,
         purchasedAt: item.purchasedAt,
-          invoiceUrl: item.invoiceUrl,
-
+        invoiceUrl: item.invoiceUrl,
       };
     });
 
