37d0d12c3fb0e1d2a4a1ac9870da2d955d75b8a5
muzeef999 <shaikmuzeef9999@gmail.com>
2026-03-23 14:51:05 +0530

    aws agent  bugs 7

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,35 +1,29 @@
 module.exports = {
   apps: [
-    {
-      name: "gateway",
-      cwd: __dirname + "/gateway",
-      script: "dist/server.js",
-      instances: "max",
-      exec_mode: "cluster",
+    { name: 'gateway',
+      cwd: __dirname + '/gateway',
+      script: 'dist/server.js',
+      instances: 'max',
+      instances: 1,
+      exec_mode: 'cluster'
     },
-
-    {
-      name: "user-service",
-      cwd: __dirname + "/services/user-service",
+    { name: 'user-service',
+      cwd: __dirname + '/services/user-service',
       script: "dist/services/user-service/src/server.js",
       instances: 1,
-      exec_mode: "fork",
+      exec_mode: 'cluster'
     },
-
-    {
-      name: "property-service",
-      cwd: __dirname + "/services/property-service",
+    { name: 'property-service',
+      cwd: __dirname + '/services/property-service',
       script: "dist/services/property-service/src/server.js",
       instances: 1,
-      exec_mode: "fork",
+      exec_mode: 'cluster'
     },
-
-    {
-      name: "payment-service",
-      cwd: __dirname + "/services/payment-service",
-      script: "dist/services/payment-service/src/server.js",
+    { name: 'payment-service',
+      cwd: __dirname + '/services/payment-service',
+      script: 'dist/server.js',
       instances: 1,
-      exec_mode: "fork",
-    },
-  ],
+      exec_mode: 'cluster'
+    }
+  ]
 };
