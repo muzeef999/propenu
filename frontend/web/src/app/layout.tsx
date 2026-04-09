0ff091e73a2cd75e43458d98cccac400d782743e
Harshad Shaik <harshadshaik200@gmail.com>
2026-04-09 17:34:54 +0530

    loading skeletons

--- frontend/web/src/app/layout.tsx
+++ frontend/web/src/app/layout.tsx
@@ -32,6 +32,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJMKXQR5');`}
+        </Script>
 
         <Script
           src="https://checkout.razorpay.com/v1/checkout.js"
@@ -44,6 +51,14 @@ export default function RootLayout({
         suppressHydrationWarning
         className={`${poppins.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJMKXQR5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
