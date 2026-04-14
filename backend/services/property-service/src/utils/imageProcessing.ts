625b977fac13a649c12f99246e76a7ef3f707a16
muzeef999 <shaikmuzeef9999@gmail.com>
2026-04-14 17:42:52 +0530

    comitted all changes  two

--- backend/services/property-service/src/utils/imageProcessing.ts
+++ backend/services/property-service/src/utils/imageProcessing.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 
 export async function createWatermarkedBuffer(imageBuffer: Buffer) {
  
-  const watermarkPath = path.resolve(__dirname, "../../../assets/watermark.png");
+const watermarkPath = path.resolve(__dirname, "../../../../assets/watermark.png");
 
   if (!fs.existsSync(watermarkPath)) {
     throw new Error("❌ Watermark not found at: " + watermarkPath);
