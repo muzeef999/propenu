3c68e82b7125c9e666f76b2708cc3c3a1c1001ea
muzeef999 <shaikmuzeef9999@gmail.com>
2026-04-15 15:13:13 +0530

    comitted new aws one

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -10,15 +10,14 @@ module.exports = {
     { name: 'user-service',
       cwd: __dirname + '/services/user-service',
       script: "dist/services/user-service/src/server.js",
-            // dist/services/user-service/src/server.js
       instances: 1,
       exec_mode: 'cluster'
     },
-    {
-      name: "email-worker",
-      script:
-        "backend/services/user-service/dist/services/user-service/src/workers/email.worker.js",
-    },
+    // {
+    //   name: "email-worker",
+    //   script:
+    //     "backend/services/user-service/dist/services/user-service/src/workers/email.worker.js",
+    // },
     { name: 'property-service',
       cwd: __dirname + '/services/property-service',
       script: "dist/services/property-service/src/server.js",
